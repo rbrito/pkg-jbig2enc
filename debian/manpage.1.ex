0f5d2915ffd339c6d1dd83af6eb4ee4c634b8ed5
Rogério Brito <rbrito@ime.usp.br>
2017-01-18 19:21:00 -0200

    debian/manpage.1.ex: Fix name and section of program.

--- debian/manpage.1.ex
+++ debian/manpage.1.ex
@@ -4,7 +4,7 @@
 .\" First parameter, NAME, should be all caps
 .\" Second parameter, SECTION, should be 1-8, maybe w/ subsection
 .\" other parameters are allowed: see man(7), man(1)
-.TH Jbig2enc SECTION "January 11 2017"
+.TH JBIG2ENC 1 "January 11 2017"
 .\" Please adjust this date whenever revising the manpage.
 .\"
 .\" Some roff macros, for reference:
